22bd952bccaa5a63cb16aada7c0286be8f673fc3
ThomasdevLC <thomas.lecam@gmail.com>
2024-06-07 14:12:49 +0200

    ex2

--- src/test/java/java17/ex01/Lambda_01_Test.java
+++ src/test/java/java17/ex01/Lambda_01_Test.java
@@ -1,5 +1,6 @@
 package java17.ex01;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Test;
 
 import java17.data.Data;
@@ -40,7 +41,10 @@ public class Lambda_01_Test {
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO result ne doit contenir que des personnes adultes (age >= 18)
-        List<Person> result = filter(personList, null);
+//        List<Person> result = filter(personList, null);
+        
+		List<Person> result = personList.stream().map(p -> p).filter(person -> person.getAge() >= 18).toList();
+
 
         assert result.size() == 83;
 
@@ -57,8 +61,10 @@ public class Lambda_01_Test {
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO result ne doit contenir que des personnes dont le prénom est "first_10"
-        List<Person> result = filter(personList, null);
-
+//        List<Person> result = filter(personList, null);
+        List<Person> result = personList.stream().filter(person -> "first_10".equals(person.getFirstname())).toList(); 
+        
+        
         assert result.size() == 1;
         assert result.get(0).getFirstname().equals("first_10");
 
@@ -75,8 +81,13 @@ public class Lambda_01_Test {
 
         // TODO result ne doit contenir que les personnes dont l'age est > 49 et dont le hash du mot de passe correspond à la valeur de la variable passwordSha512Hex
         // TODO Pour obtenir le hash d'un mot, utiliser la méthode DigestUtils.sha512Hex(mot)
-        List<Person> result = filter(personList, null);
-
+        
+//        List<Person> result = filter(personList, null);
+        
+        List<Person> result = personList.stream()
+        		.filter(person -> person.getAge() > 49)
+                .filter(person -> DigestUtils.sha512Hex(person.getPassword()).equals(passwordSha512Hex))
+                .toList();
         assert result.size() == 6;
         for (Person person : result) {
             assert person.getPassword().equals("test");
